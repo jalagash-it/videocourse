3eb5f34c192f1b6a1597bf26487f1ba245ae35cf
MyGoofyKindaBad <speedclanker@gmail.com>
2023-03-14 12:51:02 +0600

    new commit

--- src/auth/user/user.service.ts
+++ src/auth/user/user.service.ts
@@ -6,15 +6,21 @@ import { User } from './schema/user.schema';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
 
   async findOneByEmail(email: string) {
     return this.userModel.findOne({ email });
   }
 
-  async create(data: CreateUserDto, email: string, password: string): Promise<User> {
-    const user = new this.userModel(data, { email, password });
-    return user.save();
+  async create(data: CreateUserDto, email: string, password: string, user: User): Promise<User> {
+    const Createduser = new this.userModel(data, { email, password }, user);
+
+  return Createduser.save();
+
   }
-  
+
 }

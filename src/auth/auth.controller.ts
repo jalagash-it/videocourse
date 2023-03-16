592b01dddc22238be6294815b27a77ff8d849319
MyGoofyKindaBad <speedclanker@gmail.com>
2023-03-16 10:57:34 +0600

    login created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,25 @@ import { UserService } from './user/user.service';
 import { CreateUserDto } from './dto/create-user.dto/create-user.dto';
 import { User } from './user/schema/user.schema';
 import * as bcrypt from 'bcrypt';
+import { throwError } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly userService: UserService) {}
 
   @Post('login')
-  async login(@Body() body, @Session() session) {
-    // ...
+  async login(@Body() body: CreateUserDto, @Session() session) {
+    const {email, password} = body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({email, password: hashedPassword});
+
+    const UserEmail = email;
+    if (hashedPassword == password || email === UserEmail){
+      return "succesfully logined";
+    } else {
+      throw new Error("incorrect password or email");
+    }
   }
 
   @Post('register')

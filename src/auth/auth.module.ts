485c11e07a700050de11e77c4051c73c50a9d7ed
MyGoofyKindaBad <speedclanker@gmail.com>
2023-03-16 14:39:14 +0600

    changed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ModuleModule } from './module/module.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [ModuleModule],
-  providers: [UserService]
+  imports: [ModuleModule, TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+  controllers: [UserController],
 })
 export class AuthModule {}
---
+++ src/auth/user/user.modules.ts
@@ -0,0 +1,15 @@
+// user.module.ts
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [TypeOrmModule],
+})
+export class UserModule {}
